71db7c12530db24d916e2c79ec540173e1d8b9b6
Daniels <danielsbreiers@gmail.com>
2021-11-04 10:52:59 +0200

    added Completed function

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaksFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
